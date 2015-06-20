d355df69d6fced0ec7f22e8cbe819f8046f64f0b
alexax66 <alexax66@gmail.com>
2015-06-20 23:14:14 +0000

    Make possible to copy modules into output/modules/ dir

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,4 +1,20 @@
-mkdir output
+#!/bin/bash
+
+if [ ! -d $(pwd)/output ]; 
+    then
+	mkdir $(pwd)/output; 
+    fi
+
 make -C $(pwd) O=output alexax_serrano_defconfig  VARIANT_DEFCONFIG=msm8930_serrano_eur_3g_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+
 make -j5 -C $(pwd) O=output
+
 cp $(pwd)/output/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+
+if [ ! -d $(pwd)/output/modules ]; 
+    then
+	mkdir $(pwd)/output/modules; 
+    fi
+find $(pwd)/output -name '*.ko' -exec cp -v {} $(pwd)/output/modules \;
+
+exit
