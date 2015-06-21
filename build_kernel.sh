79f3051daee48da2f928cdcfce765ca2de120169
alexax66 <alexax66@gmail.com>
2015-06-21 19:54:46 +0000

    msm8930: Add serrano_eur_lte variant

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,7 +5,8 @@ if [ ! -d $(pwd)/output ];
 	mkdir $(pwd)/output; 
     fi
 
-make -C $(pwd) O=output alexax_serrano_defconfig  VARIANT_DEFCONFIG=msm8930_serrano_eur_3g_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output alexax_serrano_eur_3g_defconfig  VARIANT_DEFCONFIG=msm8930_serrano_eur_3g_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+#make -C $(pwd) O=output alexax_serrano_eur_lte_defconfig  VARIANT_DEFCONFIG=msm8930_serrano_eur_lte_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 
 make -j5 -C $(pwd) O=output
 
