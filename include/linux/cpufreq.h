4ed17a97e9de283c7c78545b18cb7477f4e0f8c3
alexax66 <alexax66@gmail.com>
2015-07-24 21:37:50 +0000

    fix some stuff

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -468,6 +468,9 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PRESERVATIVE)
+extern struct cpufreq_governor cpufreq_gov_preservative;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_preservative)
 #endif
 
 
