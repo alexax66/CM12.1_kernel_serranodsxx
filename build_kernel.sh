37267b43f028f8e61db816ac6d9dfb5563a28676
alexax66 <alexax66@gmail.com>
2015-08-07 19:59:36 +0000

    build_kernel.sh: add case between serrano-3g and serrano-lte variantes

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+case "$1" in
+         3g)
+            VARIANT="eur_3g"
+            ;;
+
+        lte)
+            VARIANT="eur_lte"
+            ;;
+
+          *)
+            VARIANT="eur_3g"
+esac
+
 if [ ! -d $(pwd)/output ]; 
     then
 	mkdir $(pwd)/output; 
     fi
 
-make -C $(pwd) O=output alexax_serrano_defconfig  VARIANT_DEFCONFIG=msm8930_serrano_eur_3g_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-#make -C $(pwd) O=output alexax_serrano_defconfig  VARIANT_DEFCONFIG=msm8930_serrano_eur_lte_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output alexax_serrano_defconfig  VARIANT_DEFCONFIG=msm8930_serrano_$VARIANT"_defconfig" SELINUX_DEFCONFIG=selinux_defconfig
 
 make -j5 -C $(pwd) O=output
 
