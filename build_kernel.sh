cb6735e48a509574d9b795a6eddde38b442bcca1
alexax66 <alexax66@gmail.com>
2015-08-07 19:44:05 +0000

    build_kernel.sh: fix 2  after previous commit

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,7 +6,7 @@ if [ ! -d $(pwd)/output ];
     fi
 
 make -C $(pwd) O=output alexax_serrano_defconfig  VARIANT_DEFCONFIG=msm8930_serrano_eur_3g_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-#make -C $(pwd) O=output alexax_serrano__defconfig  VARIANT_DEFCONFIG=msm8930_serrano_eur_lte_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+#make -C $(pwd) O=output alexax_serrano_defconfig  VARIANT_DEFCONFIG=msm8930_serrano_eur_lte_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 
 make -j5 -C $(pwd) O=output
 
