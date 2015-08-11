68501df4066d1e33240dd1ea79fd73544c2547ca
alexax66 <alexax66@gmail.com>
2015-08-11 23:22:10 +0000

    Revert "ARM: tlb: fix and clean code"
    This reverts commit a7f72b7701f3513a32bc1499723df5ab23ff562a.

--- arch/arm/include/asm/tlbflush.h
+++ arch/arm/include/asm/tlbflush.h
@@ -359,6 +359,14 @@ static inline void local_flush_tlb_all(void)
 	}
 }
 
+static inline void local_flush_tlb_all_non_is(void)
+{
+	dsb();
+	asm("mcr p15, 0, %0, c8, c7, 0" : : "r" (0));
+	dsb();
+	isb();
+}
+
 static inline void local_flush_tlb_mm(struct mm_struct *mm)
 {
 	const int zero = 0;
@@ -516,6 +524,7 @@ static inline void clean_pmd_entry(void *pmd)
 
 #ifndef CONFIG_SMP
 #define flush_tlb_all		local_flush_tlb_all
+#define flush_tlb_all_non_is	local_flush_tlb_all_non_is
 #define flush_tlb_mm		local_flush_tlb_mm
 #define flush_tlb_page		local_flush_tlb_page
 #define flush_tlb_kernel_page	local_flush_tlb_kernel_page
@@ -529,7 +538,6 @@ extern void flush_tlb_page(struct vm_area_struct *vma, unsigned long uaddr);
 extern void flush_tlb_kernel_page(unsigned long kaddr);
 extern void flush_tlb_range(struct vm_area_struct *vma, unsigned long start, unsigned long end);
 extern void flush_tlb_kernel_range(unsigned long start, unsigned long end);
-extern void flush_bp_all(void);
 #endif
 
 /*
