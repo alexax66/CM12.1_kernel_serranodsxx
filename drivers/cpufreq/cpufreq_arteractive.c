3c39b9d9ac1e884ff8ab5884e7489dcda4f4e3d8
alexax66 <alexax66@gmail.com>
2015-08-20 23:44:22 +0000

    cpufreq: cpufreq_arteractive.c: Fix compilation errors

--- drivers/cpufreq/cpufreq_arteractive.c
+++ drivers/cpufreq/cpufreq_arteractive.c
@@ -288,7 +288,7 @@ static inline void timer_set_deferrable(struct timer_list *timer)
 }
 #endif
 
-static inline cputime64_t get_cpu_idle_time(unsigned int cpu,
+static inline cputime64_t get_cpu_idle_time_art(unsigned int cpu,
 					    cputime64_t *wall)
 {
 	u64 idle_time = get_cpu_idle_time_us(cpu, wall);
@@ -309,7 +309,7 @@ static void cpufreq_interactive_timer_resched(
 
 	spin_lock_irqsave(&pcpu->load_lock, flags);
 	pcpu->time_in_idle =
-		get_cpu_idle_time(smp_processor_id(),
+		get_cpu_idle_time_art(smp_processor_id(),
 				     &pcpu->time_in_idle_timestamp);
 	pcpu->cputime_speedadj = 0;
 	pcpu->cputime_speedadj_timestamp = pcpu->time_in_idle_timestamp;
@@ -354,7 +354,7 @@ static void cpufreq_interactive_timer_start(int cpu)
 
 	spin_lock_irqsave(&pcpu->load_lock, flags);
 	pcpu->time_in_idle =
-		get_cpu_idle_time(cpu, &pcpu->time_in_idle_timestamp);
+		get_cpu_idle_time_art(cpu, &pcpu->time_in_idle_timestamp);
 	pcpu->cputime_speedadj = 0;
 	pcpu->cputime_speedadj_timestamp = pcpu->time_in_idle_timestamp;
 	spin_unlock_irqrestore(&pcpu->load_lock, flags);
@@ -501,7 +501,7 @@ static u64 update_load(int cpu)
 	struct cpufreq_loadinfo *cur_loadinfo = &per_cpu(loadinfo, cpu);
 #endif
 
-	now_idle = get_cpu_idle_time(cpu, &now);
+	now_idle = get_cpu_idle_time_art(cpu, &now);
 	delta_idle = (unsigned int)(now_idle - pcpu->time_in_idle);
 	delta_time = (unsigned int)(now - pcpu->time_in_idle_timestamp);
 
@@ -651,7 +651,7 @@ static void __cpufreq_interactive_timer(unsigned long data, bool is_notif)
 	int cpu_load;
 	struct cpufreq_interactive_cpuinfo *pcpu =
 		&per_cpu(cpuinfo, data);
-	unsigned int new_freq;
+	unsigned int new_freq = 0;
 	unsigned int loadadjfreq;
 	unsigned int index;
 	unsigned long flags;
