dc6aa7fbf408fbff0ae2994361ed0860df07704a
alexax66 <alexax66@gmail.com>
2015-10-16 08:14:14 +0000

    ASoC: msm: qdsp6: add support for any number of ASM buffers
    Current logic in ASM driver supports CPU and DSP buffers only
    when the number of buffers is power of 2. At times, the number
    of buffers might need to be different than power of 2. Modify
    the logic to find next available CPU or DSP buffer, even when
    the number of buffers is anything other than power of 2.
    
    CRs-Fixed: 771446
    Change-Id: Ibda6e0843387f6286a75c1bf4dc2068b4c4e7e9b
    Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>
    
    Based on commit: https://github.com/CyanogenMod/android_kernel_samsung_klte/commit/fca910149fa0e99a8bbbf2071336200062e1d054

--- sound/soc/msm/qdsp6/q6asm.c
+++ sound/soc/msm/qdsp6/q6asm.c
@@ -214,6 +214,14 @@ static void q6asm_session_free(struct audio_client *ac)
 	return;
 }
 
+static uint32_t q6asm_get_next_buf(uint32_t curr_buf, uint32_t max_buf_cnt)
+{
+	pr_debug("%s: curr_buf = %d, max_buf_cnt = %d\n",
+		 __func__, curr_buf, max_buf_cnt);
+	curr_buf += 1;
+	return (curr_buf >= max_buf_cnt) ? 0 : curr_buf;
+}
+
 int q6asm_audio_client_buf_free(unsigned int dir,
 			struct audio_client *ac)
 {
@@ -1143,7 +1151,8 @@ void *q6asm_is_cpu_buf_avail(int dir, struct audio_client *ac, uint32_t *size,
 		user accesses this function,increase cpu
 		buf(to avoid another api)*/
 		port->buf[idx].used = dir;
-		port->cpu_buf = ((port->cpu_buf + 1) & (port->max_buf_cnt - 1));
+		port->cpu_buf = q6asm_get_next_buf(port->cpu_buf,
+						   port->max_buf_cnt);
 		mutex_unlock(&port->lock);
 		return data;
 	}
@@ -1192,7 +1201,8 @@ void *q6asm_is_cpu_buf_avail_nolock(int dir, struct audio_client *ac,
 	 * buf(to avoid another api)
 	 */
 	port->buf[idx].used = dir;
-	port->cpu_buf = ((port->cpu_buf + 1) & (port->max_buf_cnt - 1));
+		port->cpu_buf = q6asm_get_next_buf(port->cpu_buf,
+						   port->max_buf_cnt);
 	return data;
 }
 
@@ -3807,7 +3817,8 @@ int q6asm_read(struct audio_client *ac)
 		read.uid = port->dsp_buf;
 		read.hdr.token = port->dsp_buf;
 
-		port->dsp_buf = (port->dsp_buf + 1) & (port->max_buf_cnt - 1);
+		port->dsp_buf = q6asm_get_next_buf(port->dsp_buf,
+						   port->max_buf_cnt);
 		mutex_unlock(&port->lock);
 		pr_debug("%s:buf add[0x%x] token[%d] uid[%d]\n", __func__,
 						read.buf_add,
@@ -3858,7 +3869,8 @@ int q6asm_read_nolock(struct audio_client *ac)
 		read.uid = port->dsp_buf;
 		read.hdr.token = port->dsp_buf;
 
-		port->dsp_buf = (port->dsp_buf + 1) & (port->max_buf_cnt - 1);
+		port->dsp_buf = q6asm_get_next_buf(port->dsp_buf,
+						   port->max_buf_cnt);
 		pr_info("%s:buf add[0x%x] token[%d] uid[%d]\n", __func__,
 					read.buf_add,
 					read.hdr.token,
@@ -4042,7 +4054,8 @@ int q6asm_write(struct audio_client *ac, uint32_t len, uint32_t msw_ts,
 			write.uflags = (0x00000000 | (flags & 0x800000FF));
 		else
 			write.uflags = (0x80000000 | flags);
-		port->dsp_buf = (port->dsp_buf + 1) & (port->max_buf_cnt - 1);
+		port->dsp_buf = q6asm_get_next_buf(port->dsp_buf,
+						   port->max_buf_cnt);
 
 		pr_debug("%s:ab->phys[0x%x]bufadd[0x%x]token[0x%x]buf_id[0x%x]"
 							, __func__,
@@ -4123,7 +4136,8 @@ int q6asm_write_nolock(struct audio_client *ac, uint32_t len, uint32_t msw_ts,
 			write.uflags = (0x00000000 | (flags & 0x800000FF));
 		else
 			write.uflags = (0x80000000 | flags);
-		port->dsp_buf = (port->dsp_buf + 1) & (port->max_buf_cnt - 1);
+		port->dsp_buf = q6asm_get_next_buf(port->dsp_buf,
+						   port->max_buf_cnt);
 
 		pr_debug("%s:ab->phys[0x%x]bufadd[0x%x]token[0x%x]buf_id[0x%x]"
 							, __func__,
