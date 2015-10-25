5ebb20fe9b355260da84a27bc7b54430aa316e06
alexax66 <alexax66@gmail.com>
2015-10-25 01:29:26 +0000

    cpufreq: more little tweaks for some governors

--- drivers/cpufreq/cpufreq_smartassH3.c
+++ drivers/cpufreq/cpufreq_smartassH3.c
@@ -56,7 +56,7 @@ static unsigned int awake_ideal_freq;
  * that practically when sleep_ideal_freq==0 the awake_ideal_freq is used
  * also when suspended).
  */
-#define DEFAULT_SLEEP_IDEAL_FREQ (162*1000)
+#define DEFAULT_SLEEP_IDEAL_FREQ (384*1000)
 static unsigned int sleep_ideal_freq;
 
 /*
@@ -64,7 +64,7 @@ static unsigned int sleep_ideal_freq;
  * Zero disables and causes to always jump straight to max frequency.
  * When below the ideal freqeuncy we always ramp up to the ideal freq.
  */
-#define DEFAULT_RAMP_UP_STEP (378*1000)
+#define DEFAULT_RAMP_UP_STEP (216*1000)
 static unsigned int ramp_up_step;
 
 /*
@@ -72,7 +72,7 @@ static unsigned int ramp_up_step;
  * Zero disables and will calculate ramp down according to load heuristic.
  * When above the ideal freqeuncy we always ramp down to the ideal freq.
  */
-#define DEFAULT_RAMP_DOWN_STEP (378*1000)
+#define DEFAULT_RAMP_DOWN_STEP (216*1000)
 static unsigned int ramp_down_step;
 
 /*
--- drivers/cpufreq/cpufreq_smartmax.c
+++ drivers/cpufreq/cpufreq_smartmax.c
@@ -65,7 +65,7 @@ static unsigned int awake_ideal_freq;
  * Zero disables and causes to always jump straight to max frequency.
  * When below the ideal freqeuncy we always ramp up to the ideal freq.
  */
-#define DEFAULT_RAMP_UP_STEP 200000
+#define DEFAULT_RAMP_UP_STEP 216000
 static unsigned int ramp_up_step;
 
 /*
@@ -73,7 +73,7 @@ static unsigned int ramp_up_step;
  * Zero disables and will calculate ramp down according to load heuristic.
  * When above the ideal freqeuncy we always ramp down to the ideal freq.
  */
-#define DEFAULT_RAMP_DOWN_STEP 200000
+#define DEFAULT_RAMP_DOWN_STEP 216000
 static unsigned int ramp_down_step;
 
 /*
