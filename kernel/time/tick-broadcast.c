2610aea9318ac2afcc5fdad9ec538362f8a4fb6e
alexax66 <alexax66@gmail.com>
2015-11-08 12:08:33 +0000

    Revert "clockevents: Get rid of the notifier chain"
    This reverts commit cf20e1a6deb25cddc7e74e450eed04ff112fd168.

--- kernel/time/tick-broadcast.c
+++ kernel/time/tick-broadcast.c
@@ -65,14 +65,14 @@ static void tick_broadcast_start_periodic(struct clock_event_device *bc)
 /*
  * Check, if the device can be utilized as broadcast device:
  */
-void tick_install_broadcast_device(struct clock_event_device *dev)
+int tick_check_broadcast_device(struct clock_event_device *dev)
 {
 	if ((tick_broadcast_device.evtdev &&
 	     tick_broadcast_device.evtdev->rating >= dev->rating) ||
 	     (dev->features & CLOCK_EVT_FEAT_C3STOP))
-		return;
+		return 0;
 	if (!try_module_get(dev->owner))
-		return;
+		return 0;
 
 	clockevents_exchange_device(tick_broadcast_device.evtdev, dev);
 	tick_broadcast_device.evtdev = dev;
@@ -88,7 +88,7 @@ void tick_install_broadcast_device(struct clock_event_device *dev)
 	 
 	if (dev->features & CLOCK_EVT_FEAT_ONESHOT)
 		tick_clock_notify(); */
-	return;
+	return 1;
 }
 
 /*
@@ -639,15 +639,4 @@ bool tick_broadcast_oneshot_available(void)
 	return bc ? bc->features & CLOCK_EVT_FEAT_ONESHOT : false;
 }
 
-#endif
-
-void __init tick_broadcast_init(void)
-{
-	zalloc_cpumask_var(&tick_broadcast_mask, GFP_NOWAIT);
-	zalloc_cpumask_var(&tick_broadcast_on, GFP_NOWAIT);
-	zalloc_cpumask_var(&tmpmask, GFP_NOWAIT);
-#ifdef CONFIG_TICK_ONESHOT
-	zalloc_cpumask_var(&tick_broadcast_oneshot_mask, GFP_NOWAIT);
-	zalloc_cpumask_var(&tick_broadcast_pending_mask, GFP_NOWAIT);
-	zalloc_cpumask_var(&tick_broadcast_force_mask, GFP_NOWAIT);
 #endif
