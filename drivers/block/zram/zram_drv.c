82eb09dd2f715634810424636224b5ceac005189
alexax66 <alexax66@gmail.com>
2015-11-22 21:27:14 +0000

    zram: unify error reporting
    Make zram syslog error reporting more consistent. We have random
    error levels in some places. For example, critical errors like
      "Error allocating memory for compressed page"
    and
      "Unable to allocate temp memory"
    are reported as KERN_INFO messages.
    
    a) Reassign error levels
    
    Error messages that directly affect zram
    functionality -- pr_err():
    
     Error allocating zram address table
     Error creating memory pool
     Decompression failed! err=%d, page=%u
     Unable to allocate temp memory
     Compression failed! err=%d
     Error allocating memory for compressed page: %u, size=%zu
     Cannot initialise %s compressing backend
     Error allocating disk queue for device %d
     Error allocating disk structure for device %d
     Error creating sysfs group for device %d
     Unable to register zram-control class
     Unable to get major number
    
    Messages that do not affect functionality, but user
    must be warned (because sysfs attrs will be removed in
    this particular case) -- pr_warn():
    
     %d (%s) Attribute %s (and others) will be removed. %s
    
    Messages that do not affect functionality and mostly are
    informative -- pr_info():
    
     Cannot change max compression streams
     Can't change algorithm for initialized device
     Cannot change disksize for initialized device
     Added device: %s
     Removed device: %s
    
    b) Update sysfs_create_group() error message
    
    First, it lacks a trailing new line; add it.  Second, every error message
    in zram_add() has a "for device %d" part, which makes errors more
    informative.  Add missing part to "Error creating sysfs group" message.
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Original commit: https://github.com/dorimanx/DORIMANX_LG_STOCK_LP_KERNEL/commit/71281fcb954b11663f3e0b1fb6023a9debda0617

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -519,7 +519,7 @@ static int zram_bvec_read(struct zram *zram, struct bio_vec *bvec,
 		uncmem = user_mem;
 
 	if (!uncmem) {
-		pr_info("Unable to allocate temp memory\n");
+		pr_err("Unable to allocate temp memory\n");
 		ret = -ENOMEM;
 		goto out_cleanup;
 	}
@@ -636,7 +636,7 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 	if (!handle) {
 		if (printk_timed_ratelimit(&zram_rs_time,
 					   ALLOC_ERROR_LOG_RATE_MS))
-			pr_info("Error allocating memory for compressed page: %u, size=%zu\n",
+			pr_err("Error allocating memory for compressed page: %u, size=%zu\n",
 				index, clen);
 		ret = -ENOMEM;
 		goto out;
@@ -811,7 +811,7 @@ static ssize_t disksize_store(struct device *dev,
 
 	comp = zcomp_create(zram->compressor, zram->max_comp_streams);
 	if (IS_ERR(comp)) {
-		pr_info("Cannot initialise %s compressing backend\n",
+		pr_err("Cannot initialise %s compressing backend\n",
 				zram->compressor);
 		err = PTR_ERR(comp);
 		goto out_free_meta;
@@ -1103,7 +1103,7 @@ static int create_device(struct zram *zram, int device_id)
 	 /* gendisk structure */
 	zram->disk = alloc_disk(1);
 	if (!zram->disk) {
-		pr_warn("Error allocating disk structure for device %d\n",
+		pr_err("Error allocating disk structure for device %d\n",
 			device_id);
 		ret = -ENOMEM;
 		goto out_free_queue;
@@ -1152,7 +1152,8 @@ static int create_device(struct zram *zram, int device_id)
 	ret = sysfs_create_group(&disk_to_dev(zram->disk)->kobj,
 				&zram_disk_attr_group);
 	if (ret < 0) {
-		pr_warn("Error creating sysfs group");
+		pr_err("Error creating sysfs group for device %d\n",
+				device_id);
 		goto out_free_disk;
 	}
 	strlcpy(zram->compressor, default_compressor, sizeof(zram->compressor));
@@ -1200,14 +1201,14 @@ static int __init zram_init(void)
 	int ret, dev_id;
 
 	if (num_devices > max_num_devices) {
-		pr_warn("Invalid value for num_devices: %u\n",
+		pr_err("Invalid value for num_devices: %u\n",
 				num_devices);
 		return -EINVAL;
 	}
 
 	zram_major = register_blkdev(0, "zram");
 	if (zram_major <= 0) {
-		pr_warn("Unable to get major number\n");
+		pr_err("Unable to get major number\n");
 		return -EBUSY;
 	}
 
