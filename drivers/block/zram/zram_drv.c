188c3aad73c6c0366164e576b40a5bb0a241c19f
alexax66 <alexax66@gmail.com>
2015-11-22 14:32:08 +0000

    Revert "zram: reduce the amount of memory allocation errors"
    This reverts commit 406211d86062e63c21242ca236a30a931de209e2.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -28,15 +28,8 @@
 #include <linux/string.h>
 #include <linux/vmalloc.h>
 #include <linux/err.h>
-#include <linux/ratelimit.h>
 
 #include "zram_drv.h"
-
-/*
- * We don't need to see memory allocation errors more than once every 1
- * second to know that a problem is occurring.
- */
-#define ALLOC_ERROR_LOG_RATE_MS 1000
 
 /* Globals */
 static int zram_major;
@@ -637,7 +630,6 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 	struct zcomp_strm *zstrm;
 	bool locked = false;
 	unsigned long alloced_pages;
-	static unsigned long zram_rs_time;
 
 	page = bvec->bv_page;
 	if (is_partial_io(bvec)) {
@@ -702,10 +694,8 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 
 	handle = zs_malloc(meta->mem_pool, clen);
 	if (!handle) {
-	if (printk_timed_ratelimit(&zram_rs_time,
-					ALLOC_ERROR_LOG_RATE_MS))
-				pr_info("Error allocating memory for compressed "
-							"page: %u, size=%zu\n", index, clen);
+		pr_info("Error allocating memory for compressed "
+			"page: %u, size=%zu\n", index, clen);
 		ret = -ENOMEM;
 		goto out;
 	}
