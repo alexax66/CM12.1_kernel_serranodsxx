30b02250dbe57907f4838b612e60d80319d57c59
alexax66 <alexax66@gmail.com>
2015-12-27 00:23:20 +0000

    net: tcp: check for SOCK_DEAD again in tcp_nuke_addr]
    Liping Zhang spotted a race between tcp_nuke_addr and tcp_close
    that can cause a crash. If a userspace process calls tcp_close
    on a socket at the same time that tcp_nuke_addr is closing it,
    and tcp_close wins the race to call lock_sock, it will call
    sock_orphan before releasing the lock. sock_orphan sets the
    SOCK_DEAD flag on the socket and proceeds to close it, eventually
    calling inet_csk_destroy_sock. When tcp_nuke_addr gets the socket
    lock, it calls tcp_done. But if tcp_done sees the SOCK_DEAD flag,
    it calls inet_csk_destroy_sock as well, resulting in a double free.
    
    Fix this by checking for SOCK_DEAD again after lock_sock succeeds.
    Change-Id: I0c87c3fd0598384d957b69734366bd4e2fd7e8d7
    
    Backported to 3.4.y by dorimanx.
    
    https://www.codeaurora.org/cgit/quic/la/kernel/msm/commit/?h=aosp-common/android-3.10&id=5bf7393835c06866b562ce5c3cb91ff66689b284
    
    Original commit: https://github.com/dorimanx/DORIMANX_LG_STOCK_LP_KERNEL/commit/4fd19b11929bf958c0377e5a926a6f3c30c954c8

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -3449,10 +3449,14 @@ restart:
 
 			local_bh_disable();
 			bh_lock_sock(sk);
-			sk->sk_err = ETIMEDOUT;
-			sk->sk_error_report(sk);
 
-			tcp_done(sk);
+			if (!sock_flag(sk, SOCK_DEAD)) {
+				smp_wmb();  /* be consistent with tcp_reset */
+				sk->sk_err = ETIMEDOUT;
+				sk->sk_error_report(sk);
+				tcp_done(sk);
+			}
+
 			bh_unlock_sock(sk);
 			local_bh_enable();
 			sock_put(sk);
