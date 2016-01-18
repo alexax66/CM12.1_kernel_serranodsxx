afcffe91eb6dc66c9d00abedbe2b40d2f5093a55
alexax66 <alexax66@gmail.com>
2016-01-18 16:39:53 +0000

    Revert "mm: slub: add kernel address sanitizer support for slub allocator"
    This reverts commit 5c6efcbad84ed8725b30cc20e23ce1c93c3f52ed.

--- mm/slub.c
+++ mm/slub.c
@@ -953,7 +953,6 @@ static inline void slab_post_alloc_hook(struct kmem_cache *s, gfp_t flags, void 
 	flags &= gfp_allowed_mask;
 	kmemcheck_slab_alloc(s, flags, object, slab_ksize(s));
 	kmemleak_alloc_recursive(object, s->objsize, 1, s->flags, flags);
-	kasan_slab_alloc(s, object);
 }
 
 static inline void slab_free_hook(struct kmem_cache *s, void *x)
@@ -977,7 +976,6 @@ static inline void slab_free_hook(struct kmem_cache *s, void *x)
 #endif
 	if (!(s->flags & SLAB_DEBUG_OBJECTS))
 		debug_check_no_obj_freed(x, s->objsize);
-	kasan_slab_free(s, x);
 }
 
 /*
@@ -1354,11 +1352,8 @@ static void setup_object(struct kmem_cache *s, struct page *page,
 				void *object)
 {
 	setup_object_debug(s, page, object);
-	if (unlikely(s->ctor)) {
-		kasan_unpoison_object_data(s, object);
+	if (unlikely(s->ctor))
 		s->ctor(object);
-		kasan_poison_object_data(s, object);
-	}
 }
 
 static struct page *new_slab(struct kmem_cache *s, gfp_t flags, int node)
@@ -1383,7 +1378,6 @@ static struct page *new_slab(struct kmem_cache *s, gfp_t flags, int node)
 
 	if (unlikely(s->flags & SLAB_POISON))
 		memset(start, POISON_INUSE, PAGE_SIZE << compound_order(page));
-	kasan_poison_slab(page);
 
 	last = start;
 	for_each_object(p, s, start, page->objects) {
@@ -2397,7 +2391,6 @@ void *kmem_cache_alloc_trace(struct kmem_cache *s, gfp_t gfpflags, size_t size)
 {
 	void *ret = slab_alloc(s, gfpflags, NUMA_NO_NODE, _RET_IP_);
 	trace_kmalloc(_RET_IP_, ret, size, s->size, gfpflags);
-	kasan_kmalloc(s, ret, size);
 	return ret;
 }
 EXPORT_SYMBOL(kmem_cache_alloc_trace);
@@ -2432,8 +2425,6 @@ void *kmem_cache_alloc_node_trace(struct kmem_cache *s,
 
 	trace_kmalloc_node(_RET_IP_, ret,
 			   size, s->size, gfpflags, node);
-
-	kasan_kmalloc(s, ret, size);
 	return ret;
 }
 EXPORT_SYMBOL(kmem_cache_alloc_node_trace);
@@ -2854,7 +2845,6 @@ static void early_kmem_cache_node_alloc(int node)
 	init_object(kmem_cache_node, n, SLUB_RED_ACTIVE);
 	init_tracking(kmem_cache_node, n);
 #endif
-	kasan_kmalloc(kmem_cache_node, n, sizeof(struct kmem_cache_node));
 	init_kmem_cache_node(n, kmem_cache_node);
 	inc_slabs_node(kmem_cache_node, node, page->objects);
 
@@ -3373,8 +3363,6 @@ void *__kmalloc(size_t size, gfp_t flags)
 
 	trace_kmalloc(_RET_IP_, ret, size, s->size, flags);
 
-	kasan_kmalloc(s, ret, size);
-
 	return ret;
 }
 EXPORT_SYMBOL(__kmalloc);
@@ -3418,14 +3406,12 @@ void *__kmalloc_node(size_t size, gfp_t flags, int node)
 
 	trace_kmalloc_node(_RET_IP_, ret, size, s->size, flags, node);
 
-	kasan_kmalloc(s, ret, size);
-
 	return ret;
 }
 EXPORT_SYMBOL(__kmalloc_node);
 #endif
 
-static size_t __ksize(const void *object)
+size_t ksize(const void *object)
 {
 	struct page *page;
 
@@ -3440,15 +3426,6 @@ static size_t __ksize(const void *object)
 	}
 
 	return slab_ksize(page->slab);
-}
-
-size_t ksize(const void *object)
-{
-	size_t size = __ksize(object);
-	/* We assume that ksize callers could use whole allocated area,
-	   so we need unpoison this area. */
-	kasan_krealloc(object, size);
-	return size;
 }
 EXPORT_SYMBOL(ksize);
 
